98c14e2c51901a4649f19f190a60fa3173580f5a
Meegi <manuelmiggy@gmail.com>
2023-10-27 10:38:48 +0800

    added random generator, just to fill goofy data

--- src/main/java/oop/project/libs/GenerateData.java
+++ src/main/java/oop/project/libs/GenerateData.java
@@ -1,8 +1,10 @@
 package oop.project.libs;
+import java.util.Random;
 import oop.project.lots.*;
 
 public class GenerateData {
     LotFactory lf = new LotFactory();
+    Random r = new Random();
     
     public Real_Estate simpleGenerate(){ // generates 3 blocks
         Real_Estate re = new Real_Estate();
@@ -28,6 +30,24 @@ public class GenerateData {
         re.getBlock(2).getLot(2).setStatus("Sold");
         re.getBlock(2).getLot(0).setStatus("Reserved");
         re.getBlock(2).getLot(3).setStatus("Reserved");
+        fillData(re);
         return re;
+    }
+    
+    public void fillData(Real_Estate re){
+        for (int i = 0; i < re.getBlocks().size(); i++){
+            for (int j = 0; j < re.getBlock(i).getLots().size(); j++){
+                re.getBlock(i).getLot(j).setPrice(generatePrice());
+                re.getBlock(i).getLot(j).setSize(generateSize());
+            }
+        }
+    }
+    
+    public int generatePrice(){
+        return 1000 + r.nextInt(5000);
+    }
+    
+    public int generateSize(){
+        return 100 + r.nextInt(900);
     }
 }
